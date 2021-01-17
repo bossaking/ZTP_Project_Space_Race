2b9d963f84f32b0105d6c21d6502ae4d96340a87
Bossaking <ddgamescompany@gmaiil.com>
2021-01-17 19:17:54 +0100

    Error fixed.

--- Assets/Scripts/Background/LevelStyle.cs
+++ Assets/Scripts/Background/LevelStyle.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -6,17 +7,31 @@ using UnityEngine.SceneManagement;
 public abstract class LevelStyle : MonoBehaviour
 {
 
-    private void Awake()
+    private void OnEnable()
     {
         SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+        SceneManager.activeSceneChanged += SceneManager_activeSceneChanged;
+    }
+
+    private void SceneManager_activeSceneChanged(Scene arg0, Scene arg1)
+    {
+       
+        try
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+        catch(Exception ex)
+        {
+            Debug.Log(ex.Message);
+        }
     }
 
     private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
     {
-        if (SceneManager.GetActiveScene().buildIndex == 0)
-        {
-            Destroy();
-        }
+
     }
 
     public void SetLevelStyle()
